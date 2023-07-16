a9a69a1aeb7185710e529c4a428716f45148a2a1
Adam Hawke <ah14275@my.bristol.ac.uk>
2023-07-16 15:58:47 +0100

    Displayed the shopping cart's contents

--- app/src/main/java/com/codersguidebook/store/ui/checkout/CheckoutFragment.kt
+++ app/src/main/java/com/codersguidebook/store/ui/checkout/CheckoutFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.DefaultItemAnimator
 import com.codersguidebook.store.MainActivity
+import com.codersguidebook.store.Product
 import com.codersguidebook.store.StoreViewModel
 import com.codersguidebook.store.databinding.FragmentCheckoutBinding
 
@@ -40,6 +41,43 @@ class CheckoutFragment : Fragment() {
         adapter = CheckoutAdapter(mainActivity, this)
         binding.cartRecyclerView.itemAnimator = DefaultItemAnimator()
         binding.cartRecyclerView.adapter = adapter
+
+        storeViewModel.products.observe(viewLifecycleOwner) { products ->
+            val basket = products.filter { product -> product.inCart }
+
+            if (basket.isEmpty()) binding.emptyCart.visibility = View.VISIBLE
+            else binding.emptyCart.visibility = View.GONE
+
+            when {
+                basket.size > adapter.products.size -> {
+                    val newProducts = basket - adapter.products.toSet()
+                    for (product in newProducts) {
+                        adapter.products.add(product)
+                        adapter.notifyItemInserted(adapter.products.size - 1)
+                    }
+                }
+                basket.size < adapter.products.size -> {
+                    val removedProducts = adapter.products - basket.toSet()
+                    for (product in removedProducts) {
+                        val index = adapter.products.indexOf(product)
+                        adapter.products.removeAt(index)
+                        adapter.notifyItemRemoved(index)
+                    }
+                }
+            }
+            updateOrderTotal()
+        }
+    }
+
+    fun removeProduct(product: Product) {
+        product.inCart = !product.inCart
+        val products = storeViewModel.products.value?.toMutableList() ?: return
+        val position = products.indexOf(product)
+        if (position != -1) {
+            products[position] = product
+            storeViewModel.products.value = products
+            storeViewModel.calculateOrderTotal()
+        }
     }
 
     override fun onDestroyView() {
